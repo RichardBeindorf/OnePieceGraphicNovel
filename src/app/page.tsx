2d773362f06d1d2942316a3e98f95da42e137e21
Richard Beindorf <richard_beindorf@web.de>
2024-11-11 13:00:53 +0100

    installed bundler called vite or veete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import background from "./assets/frames/backdrop/Background1.png";
 
 export default function Home() {
-	return <div>Hello OnePiece Fans!</div>;
+	return (
+		<div>
+			<div style={{ height: M }}>
+				<Image src={background} alt="Backdrop made of colorfull lightbeams" />
+			</div>
+		</div>
+	);
 }
